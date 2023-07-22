17ab52513ba558e814800701b453b888aadf4f4d
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-07-21 21:16:50 -0700

    Creating search box test - unfinished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 
 const App = () => {
 	return (
 		<div className='App'>
+			<CitySearch />
 			<EventList />
 		</div>
 	);
