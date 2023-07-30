36b5a4161939a84a95e72a4bd6b167381b32d628
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-07-30 16:37:39 -0700

    Basic Styling and dates working

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import moment from 'moment';
 
 const Event = ({ event }) => {
 	const [showDetails, setShowDetails] = useState(false);
@@ -6,12 +7,14 @@ const Event = ({ event }) => {
 		setShowDetails(!showDetails);
 	};
 
+	const eventDate = moment(event.created).format('MM/DD/yyyy h:mm A');
+
 	return (
 		<li>
 			<div className='event'>
 				<h2>{event.summary}</h2>
 				<div className='event-location'>{event.location} </div>
-				<div className='event-dateTime'>{event.created}</div>
+				<div className='event-dateTime'>{eventDate}</div>
 				{showDetails && (
 					<div data-testid='event-description' className='event-description'>
 						{event.description}
