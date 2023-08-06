b05a3d2d5902df9c09b853ce95827733deff0b8d
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-08-05 21:33:08 -0700

    numberOfEvents acceptance tests complete

---
+++ src/features/specifyNumberOfEvents.feature
@@ -0,0 +1,12 @@
+Feature: A user wants to see more or less total events in the list.
+  Scenario: When the user opens the app there should be 32 events shown by default
+    Given the app is open
+    When the user has not changed the number of events
+    Then there should be 32 events displayed in the event lsit
+
+  Scenario: User can change the number of events
+    Given the app is open and shows 32 events
+    When the user removes the number 32 from the input field
+    Then all events should no longer be displayed
+    When the user enters 5
+    Then five events should be displayed
---
+++ src/features/specifyNumberOfEvents.test.js
@@ -0,0 +1,68 @@
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
+
+// screen.logTestingPlaygroundURL();
+defineFeature(feature, (test) => {
+	beforeEach(() => {
+		render(<App />);
+	});
+
+	test('When the user opens the app there should be 32 events shown by default', ({
+		given,
+		when,
+		then,
+	}) => {
+		given('the app is open', () => {});
+
+		when('the user has not changed the number of events', () => {});
+
+		then('there should be 32 events displayed in the event lsit', async () => {
+			const numberTextBox = screen.queryByTestId('number-of-events-input');
+			expect(numberTextBox).toHaveValue(32);
+
+			await waitFor(() => {
+				const eventList = screen.getAllByRole('listitem');
+				expect(eventList).toHaveLength(32);
+			});
+		});
+	});
+
+	test('User can change the number of events', ({ given, when, then }) => {
+		given('the app is open and shows 32 events', async () => {
+			const numberTextBox = screen.queryByTestId('number-of-events-input');
+			expect(numberTextBox).toHaveValue(32);
+
+			await waitFor(() => {
+				const eventList = screen.getAllByRole('listitem');
+				expect(eventList).toHaveLength(32);
+			});
+		});
+
+		when('the user removes the number 32 from the input field', async () => {
+			const numberTextBox = screen.queryByTestId('number-of-events-input');
+			fireEvent.change(numberTextBox, { target: { value: '' } });
+		});
+
+		then('all events should no longer be displayed', async () => {
+			const numberTextBox = screen.queryByTestId('number-of-events-input');
+			await waitFor(() => {
+				expect(numberTextBox.value).toBe('');
+			});
+		});
+
+		when('the user enters 5', () => {
+			const numberTextBox = screen.queryByTestId('number-of-events-input');
+			fireEvent.change(numberTextBox, { target: { value: '5' } });
+		});
+
+		then('five events should be displayed', async () => {
+			await waitFor(() => {
+				const eventList = screen.getAllByRole('listitem');
+				expect(eventList).toHaveLength(5);
+			});
+		});
+	});
+});
