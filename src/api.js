ba1b613b08a69070033e9be564b02c101d054604
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-08-08 16:41:46 -0700

    small comment change

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ import NProgress from 'nprogress';
 /**
  *
  * @param {*} events:
- * The following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
