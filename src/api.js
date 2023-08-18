cbd6b56ba709527b44a9c2df143fa806892704ec
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-08-17 20:07:29 -0700

    offline app changes

--- src/api.js
+++ src/api.js
@@ -64,6 +64,12 @@ export const getEvents = async () => {
 		return mockData;
 	}
 
+	if (!navigator.onLine) {
+		const events = localStorage.getItem('lastEvents');
+		NProgress.done();
+		return events ? JSON.parse(events) : [];
+	}
+
 	const token = await getAccessToken();
 	if (token) {
 		removeQuery();
@@ -72,6 +78,7 @@ export const getEvents = async () => {
 		const response = await fetch(url);
 		const result = await response.json();
 		if (result) {
+			localStorage.setItem('lastEvents', JSON.stringify(result.events));
 			return result.events;
 		} else return null;
 	}
